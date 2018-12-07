9d896809995f6e1397ecad3ec90045cad94584de
Amir Behnam <amir.behnam@gmail.com>
2018-12-07 14:09:00 -0500

    Add example to reproduce error with uniq indices

--- src/main/resources/db/migration/V1__initialise_database.sql
+++ src/main/resources/db/migration/V1__initialise_database.sql
@@ -1,5 +1,6 @@
- CREATE TABLE mcve.test (
+CREATE TABLE mcve.test (
   id    INT NOT NULL AUTO_INCREMENT,
   value INT,
+  UNIQUE INDEX `value_un` (`value`),
   CONSTRAINT pk_test PRIMARY KEY (id)
 );
