b24209d7fc5a9fe0005d2e79612a1ce7eeae22d4
Amir Behnam <amir.behnam@gmail.com>
2018-12-10 10:53:01 -0500

    Example to reproduce error

--- src/main/resources/db/migration/V1__initialise_database.sql
+++ src/main/resources/db/migration/V1__initialise_database.sql
@@ -1,5 +1,14 @@
- CREATE TABLE mcve.test (
-  id    INT NOT NULL AUTO_INCREMENT,
-  value INT,
-  CONSTRAINT pk_test PRIMARY KEY (id)
+CREATE TABLE `foo` (
+  `id`     BIGINT(20) NOT NULL,
+  `bar_id` BIGINT(20) NOT NULL,
+
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`bar_id`) REFERENCES `bar` (`id`)
+);
+
+CREATE TABLE `bar` (
+  `id`   BIGINT(20)    NOT NULL,
+  `name` VARCHAR(2500) NOT NULL,
+
+  PRIMARY KEY (`id`)
 );
